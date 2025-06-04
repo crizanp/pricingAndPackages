d44a5245437ba02bc791356cde32ed9a9b4e802f
crijanp <srij89995@gmail.com>
2025-06-04 16:26:38 +0545

    tag meta added

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="google-site-verification" content="4g0gsGgPJ_73i_h7GIUhnXYddap4FQI0M6axwX0gkT0" />
         {/* Google AdSense */}
        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8377837851676312"
      crossorigin="anonymous"></script>
