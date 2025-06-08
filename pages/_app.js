21d0f7e9bb4b62c1793d9695a8ab0a3c99a28c6b
crijanp <srij89995@gmail.com>
2025-06-08 11:51:31 +0545

    favicon updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
 // pages/_app.js
 import '../styles/globals.css';
 import Layout from '../components/Layout';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+       <link rel="shortcut icon" href="/images/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
