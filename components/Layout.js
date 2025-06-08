c34485344210b593fb885e34ce9ed523ad4bdf59
crijanp <srij89995@gmail.com>
2025-06-08 11:44:43 +0545

    favicon updated

--- components/Layout.js
+++ components/Layout.js
@@ -13,8 +13,9 @@ const Layout = ({
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
-        <link rel="icon" type="image/png" href="/images/favicon.png" />
-      </Head>
+ <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+              </Head>
 
       <Navbar/>
       <main className="pt-16">{children}</main>
--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
+         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        
         <meta name="google-site-verification" content="4g0gsGgPJ_73i_h7GIUhnXYddap4FQI0M6axwX0gkT0" />
         {/* Google AdSense */}
        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8377837851676312"
